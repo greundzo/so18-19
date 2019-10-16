bd3874259564ada39b3aafac5b5b606565d452fc
Greundzo <niccolo.alleyson@edu.unito.it>
2019-10-16 17:48:26 +0200

    Shared memory funzionante anche nel file student.

--- src/student.c
+++ src/student.c
@@ -4,6 +4,7 @@
 #include "shared.h"
 
 int matricola, voto_AdE, nof_elements, nof_invites, max_reject;
+int mem_id, sem_id;
 struct sigaction killed;
 
 void killhandler (int signal) {
@@ -27,24 +28,24 @@ int main(int argc, char ** argv)
         TEST_ERROR
     }
 
-    pStudentData = (shared *)connect(memid);
+    mem_id = create_memory();
+    pStudentData = (shared *)connect(mem_id);
     voto_AdE = generate_random_integer(18, 30, getpid());
-    int sem = create_sem();
-    /* CRITICAL AREA
+    sem_id = create_sem();
+    // CRITICAL AREA
     ops.sem_num = 1;
     ops.sem_op = 0;
-    int sem = create_sem();
-    if (semop(sem, &ops, 1) == -1) {
+    if (semop(sem_id, &ops, 1) == -1) {
         TEST_ERROR
-    }*/
+    }
 
     
-    take_sem(sem, 0);    
+    take_sem(sem_id, 0);    
     printf("%d\n", voto_AdE);
-    //int index = pStudentData->pc;
-    //pStudentData->stdata[index].student_pid = getpid();
-    //pStudentData->pc ++;    
-    release_sem(sem, 0);
+    int index = pStudentData->pc;
+    pStudentData->stdata[index].student_pid = getpid();
+    pStudentData->pc ++;    
+    release_sem(sem_id, 0);
 
     exit(0);
 }
